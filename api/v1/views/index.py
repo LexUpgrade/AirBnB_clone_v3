eb9adf3c1d50ab507a23402a7158c016c044e6a4
LexUpgrade <alexanderedim89@gmail.com>
2024-08-21 16:11:39 +0100

    Added a new route to retrieve the number of all each objects by their type from storage

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,27 @@
 """Creates a 'status' route."""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"Amenity": Amenity, "City": City, "Place": Place, "Review": Review,
+           "State": State, "User": User}
 
 
 @app_views.route("/status", strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """Retrieves the number of each objects by type in the storage."""
+    keys = ['amenities', 'cities', 'places', 'reviews', 'states', 'users']
+    response_data = {data[0]: storage.count(data[1])
+                     for data in zip(keys, classes.values())}
+    return jsonify(**response_data)
