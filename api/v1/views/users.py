84bb4e3cf4d93d6d0d5f1bc95843d416ef803ff7
LexUpgrade <alexanderedim89@gmail.com>
2024-08-22 21:40:03 +0100

    Created route for methods ['GET', 'DELETE', 'POST', 'PUT'] and handle errors accordingly

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -8,7 +8,9 @@ from flask import jsonify, abort, make_response, request
 
 @app_views.route("/users", methods=['GET'], strict_slashes=False)
 def get_users():
-    """Gets a list of all 'User' objects from storage."""
+    """
+    Gets a list of all 'User' objects from storage.
+    """
     users = storage.all(User).values()
     users_list = [user.to_dict() for user in users]
     return jsonify(users_list)
@@ -17,7 +19,9 @@ def get_users():
 @app_views.route("users/<user_id>",
                  methods=['GET'], strict_slashes=False)
 def get_user_id(user_id):
-    """Gets a specific user from the storage."""
+    """
+    Gets a specific user from the storage.
+    """
     user = storage.get(User, user_id)
     if not user:
         abort(404)
@@ -27,7 +31,9 @@ def get_user_id(user_id):
 @app_views.route("users/<user_id>",
                  methods=['DELETE'], strict_slashes=False)
 def delete_user_id(user_id):
-    """Deletes a 'User' object by an id."""
+    """
+    Deletes a 'User' object by an id.
+    """
     user = storage.get(User, user_id)
     if not user:
         abort(404)
@@ -38,7 +44,9 @@ def delete_user_id(user_id):
 
 @app_views.route("/users", methods=['POST'], strict_slashes=False)
 def post_user():
-    """Creates a new 'User' object."""
+    """
+    Creates a new 'User' object.
+    """
     data = request.get_json(silent=True)
     if not data:
         abort(400, description="Not a JSON")
@@ -56,8 +64,8 @@ def post_user():
 @app_views.route("/users/<user_id>",
                  methods=['PUT'], strict_slashes=False)
 def put_user(user_id):
-    """Updates an existing 'User' object, otherwise handle errors
-    accordingly.
+    """
+    Updates an existing 'User' object, otherwise handle errors accordingly.
     """
     user = storage.get(User, user_id)
     data = request.get_json(silent=True)
